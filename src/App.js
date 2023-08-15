2245ede8d42cf0be56d6d0216e28e39baf186091
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-08-16 00:51:36 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import SignUp from './signupPage/sign-up';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
+import AuthPage from './pages/AuthPage';
+import Profile from './pages/profile';
+import ProfileForm from './Profile/ProfileForm';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <SignUp />
-    </div>
-  );
+  const authCtx = useContext(AuthContext);
+  return(
+    <>
+    <Switch>
+      {!authCtx.isLoggedIn && (<Route path='/auth'>
+        <AuthPage />
+      </Route>)}
+
+      <Route path='/profile'>
+        {authCtx.isLoggedIn && <Profile />}
+        {!authCtx.isLoggedIn && <Redirect to='/auth'/>}
+      </Route>
+
+      <Route path="/profileForm">
+        <ProfileForm />
+      </Route>
+
+      <Route path="*">
+        <Redirect to="/auth" />
+      </Route>
+    </Switch>
+    </>
+  )
 }
 
 export default App;
