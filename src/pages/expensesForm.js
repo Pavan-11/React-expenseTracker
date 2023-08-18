627f1d383b224907ff12a76e94dfd09fda3f9afc
Pavan-11 <121480038+Pavan-11@users.noreply.github.com>
2023-08-18 23:46:29 +0530

    delete and edit buttons

--- src/pages/expensesForm.js
+++ src/pages/expensesForm.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import classes from './expenseForm.module.css';
+import { Button } from 'react-bootstrap';
 
 const ExpensesForm = () => {
     const [expenses, setExpenses] = useState([]);
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('fuel');
+    const [editingExpenseId, setEditingExpenseId] = useState(null);
 
 
     useEffect(() => {
 
         fetchExpenses();
-    },[]);
+    }, []);
 
-    const fetchExpenses = async() => {
-        try{
+    const fetchExpenses = async () => {
+        try {
             const response = await axios.get('https://expensetracker-d7655-default-rtdb.firebaseio.com/expenses.json');
-            if(response.data){
+            if (response.data) {
                 const expenseList = Object.values(response.data);
                 setExpenses(expenseList);
             }
-        }catch(error){
+        } catch (error) {
             console.error('Error fetching expenses:', error);
         }
     };
 
-    const addExpense = async(event) => {
+    const addExpense = async (event) => {
         event.preventDefault();
         const newExpense = {
             description,
@@ -34,14 +36,56 @@ const ExpensesForm = () => {
             category
         };
 
-        try{
+        try {
             await axios.post('https://expensetracker-d7655-default-rtdb.firebaseio.com/expenses.json', newExpense);
             setDescription('');
             setPrice('');
             setCategory('fuel');
             fetchExpenses();
-        }catch(error){
-            console.error('Error adding expense:',error)
+        } catch (error) {
+            console.error('Error adding expense:', error)
+        }
+    };
+
+    const deleteExpense = async (id) => {
+        console.log('delete clicked')
+        try {
+            await axios.delete(`https://expensetracker-d7655-default-rtdb.firebaseio.com/expenses/${id}.json`);
+            fetchExpenses();//fetching updated expenses after deleting
+        } catch (error) {
+            console.error('Error deleting expense: ', error);
+        }
+    }
+
+    const editExpense = (id) => {
+        setEditingExpenseId(id);
+        const expenseToEdit = expenses.find(expense => expense.id === id);
+        setDescription(expenseToEdit.description);
+        setPrice(expenseToEdit.price);
+        setCategory(expenseToEdit.category);
+    };
+
+    const cancelEdit = () => {
+        setEditingExpenseId(null);
+        setDescription('')
+        setCategory('fuel')
+        setPrice('');
+    };
+
+
+    const saveEdit = async () => {
+        const editedExpense = {
+            description,
+            price: parseFloat(price),
+            category
+        };
+
+        try {
+            await axios.put(`https://expensetracker-d7655-default-rtdb.firebaseio.com/expenses/${editingExpenseId}.json`, editedExpense)
+            cancelEdit();
+            fetchExpenses();//fetch updated expense after editing
+        } catch (error) {
+            console.error('Error editing expense: ', error);
         }
     };
 
@@ -85,20 +129,33 @@ const ExpensesForm = () => {
 
             <div>
                 <table className={classes.table}>
-                <h2>Expenses entered by the user:</h2>
+                    <h2>Expenses entered by the user:</h2>
                     <thead>
                         <tr className={classes.tr}>
-                            <th style={{padding : '1rem 10rem'}}>Description</th>
+                            <th style={{ padding: '1rem 10rem' }}>Description</th>
                             <th style={{ padding: '1rem 10rem' }}>Price</th>
                             <th style={{ padding: '1rem 10rem' }}>Category</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {expenses.map((expense, index) => (
-                            <tr key={index} className={classes.tr}>
-                                <td style={{ padding: '1rem 10rem' }}>{expense.description}</td>
-                                <td style={{ padding: '1rem 10rem' }}>{expense.price}</td>
-                                <td style={{ padding: '1rem 10rem' }}>{expense.category}</td>
+                        {Object.entries(expenses).map(([expenseId, expense]) => (
+                            <tr key={expenseId} className={classes.tr}>
+                                <td style={{ padding: '1rem 10rem', position: 'relative', left: '2rem' }}>{expense.description}</td>
+                                <td style={{ padding: '1rem 10rem', position: 'relative', left: '4rem' }}>{expense.price}</td>
+                                <td style={{ padding: '1rem 10rem', position: 'relative', left: '5rem' }}>{expense.category}</td>
+                                <td>
+                                    {editingExpenseId === expenseId ? (
+                                        <div>
+                                            <Button variant="secondary" size="sm" onClick={() => cancelEdit()}>Cancel</Button>
+                                            <Button variant="success" size="sm" onClick={() => saveEdit()}>Save</Button>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <Button className='m-3' size="sm" onClick={() => editExpense(expenseId)}>Edit</Button>
+                                            <Button size="sm" onClick={() => deleteExpense(expenseId)}>Delete</Button>
+                                        </div>
+                                    )}
+                                </td>
                             </tr>
                         ))}
                     </tbody>
